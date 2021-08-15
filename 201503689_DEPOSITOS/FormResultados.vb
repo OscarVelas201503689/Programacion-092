471c76452150ca621e754aaa20f3a1c846f1e97e
Oscar Amilcar Velas <88955194+OscarVelas201503689@users.noreply.github.com>
2021-08-14 21:45:46 -0600

    bo

--- 201503689_DEPOSITOS/FormResultados.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class FormResultados
-    Private Sub Btn_Regresar_Click(sender As Object, e As EventArgs) Handles Btn_Regresar.Click
-        Me.Hide()
-        Form1.Show()
-    End Sub
-End Class
